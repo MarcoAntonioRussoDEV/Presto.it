5649db433248e08cddb17e173897697207bd1b57
Antonio Pezzino <antopez1984@gmail.com>
2024-08-05 21:43:34 +0200

    aggiunta bootstrap

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PublicController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', [PubblicController::class, 'homepage'])->name('homepage');
