b22dd7d0baf33bdb06a46f8cbb159b649ffa11cf
Marco Antonio Russo <ocra94@gmail.com>
2024-08-06 18:50:14 +0200

    create pagine di autenticazione

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="image-container">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+            </div>
+        </div>
+    </div>
+</x-layout>
