ae0465acbdf1c0f29457d3d4fc5d917eec465f91
Antonio Pezzino <antopez1984@gmail.com>
2024-08-06 21:21:36 +0200

    aggiunta cartella article e livewire

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PublicController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
+Route::get('/create/article', [ArticleController::class, 'create'])->name('create.article');
