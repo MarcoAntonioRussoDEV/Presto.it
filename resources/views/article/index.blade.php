19d057f4ced185a065a745a25a90e49100c45d90
Antonio Pezzino <antopez1984@gmail.com>
2024-08-07 21:26:09 +0200

    modifiche errori

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,22 +5,22 @@
      </div>    
  </div>
     <div class="row height-custom justify-content-center align-items-center py-5">
-           @forelse($articles as $article)
-        <div class="col-12 col md-3">
-            <x-card:article="$article" />
-      </div>
+        @forelse($articles as $article)
+            <div class="col-12 col md-3">
+                <x-card :article="$article" />
+            </div>
          @empty
-       <div class="col-12">
-            <h3 class="text-center">
+            <div class="col-12">
+                <h3 class="text-center">
                 Non sono ancora stati creati articoli
-          </h3>
+                </h3>
          @endforelse
       </div>
   </div>   
      <div class="d-flex justify-content-center">
           <div>
-            {{$articles->links()}}
+            {{ $articles->links() }}
          </div>
     </div>
-</ layout>
+</x-layout>
         
