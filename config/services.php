c8c29e0368f47c44ea26a7f23b0282139123d087
Dennis Torre <dnstorre@gmail.com>
2024-08-09 18:56:58 +0200

    correzione

--- config/services.php
+++ config/services.php
@@ -39,19 +39,8 @@ return [
     'client_id' => env('GOOGLE_CLIENT_ID'),
     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
     'redirect' => env('GOOGLE_REDIRECT_URL'),
-    ]
+    ],
 
-    'google' => [
-    'client_id' => env('GOOGLE_CLIENT_ID'),
-    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => env('GOOGLE_REDIRECT_URI'),
-],
-
-
-    'google' => [
-   'client_id' => env('GOOGLE_CLIENT_ID'),
-   'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-   'redirect' => env('GOOGLE_REDIRECT_URL'),
-   ]
+   
 
 ];
