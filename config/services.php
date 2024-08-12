c9499fb0a367cbe89c855b2abe5065baf0b6926d
Antonella Frappampina <antonella.frappy@live.it>
2024-08-12 19:02:31 +0200

    ...

--- config/services.php
+++ config/services.php
@@ -41,11 +41,7 @@ return [
     'redirect' => env('GOOGLE_REDIRECT_URL'),
     ]
 
-    'google' => [
-    'client_id' => env('GOOGLE_CLIENT_ID'),
-    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => env('GOOGLE_REDIRECT_URI'),
-],
+   
 
 
 ];
