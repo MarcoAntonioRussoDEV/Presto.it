16ecfecb83448274ce3631c152b7931ae28f3b3b
Antonio Pezzino <antopez1984@gmail.com>
2024-08-19 20:55:08 +0200

    user-story 10 completata

--- resources/views/article/searched.blade.php
+++ resources/views/article/searched.blade.php
@@ -8,7 +8,7 @@
         <div class="row height-custom justify-content-center align-items-center py-5">
             @forelse ($articles as $article)
                 <div class="col-12 col-md-3">
-                    <x-card :article="$article" />
+                    <x-article-card :article="$article" />
                 </div>
             @empty
                 <div class="class-12">
@@ -21,7 +21,7 @@
     </div>
     <div class="d-flex justify-content-center">
         <div>
-            {{ $article->links() }}
+            {{ $articles->links() }}
         </div>
     </div>
 
