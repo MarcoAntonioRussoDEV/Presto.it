cbe91c0be3202795df1155aff8131aad9f514ce4
Marco Antonio Russo <ocra94@gmail.com>
2024-08-30 20:25:48 +0200

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -17,14 +17,14 @@
       </div>
   </div>
 
-  @else
+  @elseif(Auth::user() && Auth::user()->is_revisor)
   <div class="container p-4">
     <div class="row align-items-center border border-primary rounded rounded-xl p-3">
       <div class="col-lg-6 col-md-12 d-flex justify-content-center mb-2 mb-lg-0 custom-footer-img-h">
         <img src="{{url("asset/img/revisor-footer-img.svg")}}" alt="">
       </div>
       <div class="col-lg-6 col-md-12 text-center">
-        <h5 class="text-uppercase">{{__("ui.youAreARevisor")}}</h5>
+        <h5 class="text-uppercase">{{__("ui.youAreARevisor")}}!</h5>
         <p>
           {{__("ui.thanksForYourWorkOnPresto")}}
         </p>
